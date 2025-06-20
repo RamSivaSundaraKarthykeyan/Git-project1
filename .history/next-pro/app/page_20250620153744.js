d2bdcac46a4de1f06649c98018616d86fcd3b1e4
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-06-20 16:13:27 +0530

    bg image is added

---
+++ .history/next-pro/app/page_20250620153744.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-custom-background bg-cover bg-center">
+      <h1 className="text-white text-4xl font-bold text-center pt-20">
+        Welcome to My Website
+      </h1>
+    </div>
+  );
+}
