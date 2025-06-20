748f8d57a11ba9ac90773b24a2cc572e65bcf72d
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-06-20 19:50:39 +0530

    The design is not coming

---
+++ .history/next-pro/app/(components)/NavBar_20250620193700.js
@@ -0,0 +1,36 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="">
+        <div className="flex justify-center justify-evenly">
+          <div className="bg-black bg-opacity-80 backdrop-blur-md px-8 py-4 flex items-center justify-between">
+            <picture>
+              <Link href="/">
+                <Image
+                  src="/image-removebg-preview.png" // do not include public in the src address it will cause the to not load and just presents a png logo
+                  alt="logo"
+                  height={50}
+                  width={50}
+                ></Image>
+              </Link>
+            </picture>
+          </div>
+          <div className="flex justify-center my-3 gap-9">
+            <Link href="/invest">Invest</Link>
+            <Link href="/raise">Raise</Link>
+            <Link href="/about">About</Link>
+          </div>
+          <div>
+            <Link href="/about">
+              <button className=" p-2 rounded-b-xl my-2">Get Started</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ .history/next-pro/app/(components)/NavBar_20250620194622.js
@@ -0,0 +1,44 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="">
+        <div className="flex justify-center justify-evenly">
+          <div className="bg-black bg-opacity-80 backdrop-blur-md px-8 py-4 flex items-center justify-between">
+            <picture>
+              <Link href="/">
+                <Image
+                  src="/image-removebg-preview.png" // do not include public in the src address it will cause the to not load and just presents a png logo
+                  alt="logo"
+                  height={50}
+                  width={50}
+                ></Image>
+              </Link>
+            </picture>
+          </div>
+          <div className="flex gap-6 text-white text-sm justify-center my-3 font-medium">
+            <Link href="/invest" className="hover:underline">
+              Invest
+            </Link>
+            <Link href="/raise" className="hover:underline">
+              Raise
+            </Link>
+            <Link href="/about" className="hover:underline">
+              About
+            </Link>
+          </div>
+          <div>
+            <Link href="/about">
+              <button className="px-4 py-2 rounded-full bg-gray-700 text-white hover:bg-gray-600 my-2">
+                Get Started
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
