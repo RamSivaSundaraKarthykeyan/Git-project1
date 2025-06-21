57a80031fa8ea55eb9df31df9275db686add25bd
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-06-21 11:34:59 +0530

    Spacings for the button like element is perfect now

---
+++ .history/next-pro/app/(components)/LandHead_20250621112947.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+const LandHead = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center space-x-2 bg-gray-50/5 mx-197 py-2 rounded-lg border-1 border-zinc-900">
+        <Image src="/bgr-logo.png" width={40} height={40} alt=""></Image>
+        <p>Partner with Europe's Best Investors</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandHead;
