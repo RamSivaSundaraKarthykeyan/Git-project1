0da2e390883caf1cde08d4001ef5c035a6055fac
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-06-21 12:42:30 +0530

    Now the added elements appear the same in all device sizes

---
+++ .history/next-pro/app/(components)/LandHead_20250621115305.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+const LandHead = () => {
+  return (
+    <div className="mt-40">
+      <div className="flex items-center justify-center space-x-2 bg-gray-50/5 mx-197 mb-10 py-2 rounded-lg border-1 border-zinc-900">
+        <Image src="/bgr-logo.png" width={20} height={20} alt=""></Image>
+        <p>Partner with Europe's Best Investors</p>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-6xl mb-1 font-bold">Join Europe's</p>
+        <p className="break-after-column md:break-after-auto text-6xl font-bold text-weborange">
+          Top Startup Backers
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LandHead;
