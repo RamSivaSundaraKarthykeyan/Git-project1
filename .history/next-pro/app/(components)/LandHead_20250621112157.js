240ce2c648f3c727385a12f435b96c38ddede3d6
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-06-21 11:29:18 +0530

    added border for all elements with border, created the button like element in the body of the website

---
+++ .history/next-pro/app/(components)/LandHead_20250621112157.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+const LandHead = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center space-x-0 bg-gray-50/5 mx-195">
+        <Image src="/bgr-logo.png" width={40} height={20} alt=""></Image>
+        <p>Partner with Europe's Best Investors</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandHead;
---
+++ .history/next-pro/app/(components)/LandHead_20250621112257.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+const LandHead = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center space-x-2 bg-gray-50/5 mx-195 py-2.5">
+        <Image src="/bgr-logo.png" width={20} height={20} alt=""></Image>
+        <p>Partner with Europe's Best Investors</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandHead;
