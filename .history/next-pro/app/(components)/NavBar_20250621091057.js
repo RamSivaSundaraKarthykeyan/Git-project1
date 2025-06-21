725967e61cca161f9334faf4ad718c53575c8915
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-06-21 09:43:30 +0530

    The right button is now spaced perfectly

---
+++ .history/next-pro/app/(components)/NavBar_20250621091057.js
@@ -0,0 +1,44 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="w-full">
+        <div className="mx-30 my-9.5 grid grid-cols-3 gap-150 rounded-2xl bg-gray-50/5 justify-evenly">
+          <div className=" items-start g px-6 py-3 text-white text-sm font-medium ">
+            <picture>
+              <Link href="/">
+                <Image
+                  src="/image-removebg-preview.png" // do not include public in the src address it will cause the to not load and just presents a png logo
+                  alt="logo"
+                  height={50}
+                  width={50}
+                ></Image>
+              </Link>
+            </picture>
+          </div>
+          <div className="flex gap-x-14 pt-4 text-white text-sm justify-center my-3 font-medium">
+            <Link href="/invest" className="hover:underline">
+              Invest
+            </Link>
+            <Link href="/raise" className="hover:underline">
+              Raise
+            </Link>
+            <Link href="/about" className="hover:underline">
+              About
+            </Link>
+          </div>
+          <div>
+            <Link href="/about">
+              <button className="px-4 my-2.5 rounded-md py-5 bg-gray-700 hover:bg-gray-600">
+                Get Started {">>"}
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
